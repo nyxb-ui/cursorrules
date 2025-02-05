8730d1e466bc71ecb960ea9f6f961fd80e57dc1f
nyxb <dennis.ollhoff@joypix.gg>
2025-02-05 15:22:24 +0100

    🧹 chore(config): remove environment variables from next.config.mjs and nixpacks.toml
    The environment variables have been removed from the configuration files to centralize and manage them more effectively, likely through a different mechanism or file. This change helps in maintaining a cleaner configuration and reduces redundancy.

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    output: "standalone",
-   env: {
-      UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
-      UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
-      NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-   },
 }
 
 export default nextConfig
