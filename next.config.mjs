bea00066f970098087cc25c9443e29144b5ad5ed
nyxb <dennis.ollhoff@joypix.gg>
2025-02-05 15:21:10 +0100

    🧹 chore(next.config.mjs): remove experimental outputFileTracingRoot configuration to clean up unused configuration options

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    output: "standalone",
-   experimental: {
-      outputFileTracingRoot: process.cwd(),
-   },
    env: {
       UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
       UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
