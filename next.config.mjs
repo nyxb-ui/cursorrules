e39694c563ff81eb565ce50949588f791284c061
nyxb <dennis.ollhoff@joypix.gg>
2025-02-05 15:08:14 +0100

    ✨ feat(next.config.mjs): add environment variables for Upstash Redis and app URL to support dynamic configuration
    🛠️ build(nixpacks.toml): set NEXT_PUBLIC_APP_URL environment variable during build phase to ensure correct app URL is used

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    output: "standalone",
+   experimental: {
+      outputFileTracingRoot: process.cwd(),
+   },
+   env: {
+      UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+      UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
+      NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+   },
 }
 
 export default nextConfig
